a3cefabebcbac8f93f15afeae980da6f2d596946
Kai Quach <kaiq@iastate.edu>
2024-11-03 01:08:41 -0600

    Update MyBrowse.js

--- a3/src/MyBrowse.js
+++ a3/src/MyBrowse.js
@@ -1,1 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from 'react';
+
+function ShowProducts(){
+    const [catalog, setCatalog] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () =>{
+            const response = await fetch("/products.json");
+            const data = await response.json();
+      
+            setCatalog(data);
+      
+      
+          };
+    }, []);
+}
