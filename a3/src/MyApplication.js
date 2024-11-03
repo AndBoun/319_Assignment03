ba9db3bfe969741134722fe4217d9f935362d1c9
AndBoun <167598896+AndBoun@users.noreply.github.com>
2024-11-03 00:24:25 -0500

    formatting

--- a3/src/MyApplication.js
+++ a3/src/MyApplication.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import Shop from "./Shopping"
 import Payment from "./MyPayment";
 import Summary from "./MySummary";
-function App(){
+function App() {
     const [dataF, setDataF] = useState({});
     const [viewer, setViewer] = useState(0);
-   
-    return(<div> 
-        {viewer === 0 && <Payment dataF={dataF} setDataF={setDataF} viewer={viewer} setViewer={setViewer} />}
-        {viewer === 1 && <Summary dataF={dataF} setDataF={setDataF} viewer={viewer} setViewer={setViewer} />}
-    </div>);
+
+    return (
+        <div>
+            {viewer === 0 && <Payment dataF={dataF} setDataF={setDataF} viewer={viewer} setViewer={setViewer} />}
+            {viewer === 1 && <Summary dataF={dataF} setDataF={setDataF} viewer={viewer} setViewer={setViewer} />}
+        </div>
+    );
 }
 export default App; 
